7ee53b2fe2ddc9537a926c41eb1d6cad968a2e5c
Wade Baglin <plebmail@gmail.com>
2022-06-07 10:35:27 +1000

    Update example

--- index.ts
+++ index.ts
@@ -8,6 +8,8 @@ import {
 import { MongoMemoryServer } from 'mongodb-memory-server-core';
 import { IObjectWithTypegooseFunction } from "@typegoose/typegoose/lib/types";
 
+// Read about query methods here https://typegoose.github.io/typegoose/docs/api/decorators/query-method
+
 interface QueryHelpers {
   findByName: types.AsQueryMethod<typeof findByName>;
 }
@@ -53,37 +55,33 @@ class Person {
   await PersonModel.create({ name: 'n1', addresses: [{ line1: 'a1' }, { line1: 'a2' }] })
   await PersonModel.create(new Person('n2', [new Address('a3'), new Address('a4')]));
 
-  console.log('Find one lean POJO')
+  console.log('👇 Find one lean POJO 👇')
   let pojo = await PersonModel.findOne().lean()
   console.log(pojo, pojo.addresses[0].line1)
 
-  console.log('Find all docs')
+  console.log('👇 Find all docs 👇')
   const allDocs =  await PersonModel.find().lean()
 
   for (let doc of allDocs) {
-    // For when addresses is a sub-doc
-    // let addresses = (await doc).addresses;
-    // console.log(doc), addresses)
+    // For when addresses are a sub-doc
+    // console.log(doc), (await doc).addresses[0].line2)
 
     // For when addresses are a mixed type
-    console.log(doc, (await doc).addresses[0].line1)
+    console.log(doc, doc.addresses[0].line1)
   }
 
-  console.log('Find docs with name of `n1`')
+  console.log('👇 Find docs with name of `n1` 👇')
   const docs = await PersonModel.find().findByName('n1');
 
   for (let doc of docs) {
-    // For when addresses is a sub-doc
-    // let addresses = (await doc).addresses;
-    // console.log(doc), addresses)
+    // For when addresses are a sub-doc
+    // console.log(doc), (await doc).addresses[0].line2)
 
     // For when addresses are a mixed type
-
-    // Type error
-    // console.log(doc, (await doc).addresses[0].line1)
-
+    // ⚠️ Type error ⚠️
+    // console.log(doc, doc.addresses[0].line1)
     const docCasted = (doc as any) as mongoose.Document<any, QueryHelpers> & Person & IObjectWithTypegooseFunction & { _id: (mongoose.Document<any, QueryHelpers> & Person & IObjectWithTypegooseFunction)["_id"] }
-    console.log(doc, (await docCasted).addresses[0].line1)
+    console.log(doc, docCasted.addresses[0].line1)
   }
 
   process.exit(0)
