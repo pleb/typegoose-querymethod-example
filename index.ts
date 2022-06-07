c57b1eb80f85b4376f7ee36e89287aa7668d79ea
Wade Baglin <plebmail@gmail.com>
2022-06-07 10:26:41 +1000

    Update example

--- index.ts
+++ index.ts
@@ -1,60 +1,90 @@
 import {
-    types,
-    queryMethod,
-    prop,
-    getModelForClass,
-    mongoose
+  types,
+  queryMethod,
+  prop,
+  getModelForClass,
+  mongoose
 } from '@typegoose/typegoose';
 import { MongoMemoryServer } from 'mongodb-memory-server-core';
+import { IObjectWithTypegooseFunction } from "@typegoose/typegoose/lib/types";
 
 interface QueryHelpers {
-    findByName: types.AsQueryMethod<typeof findByName>;
+  findByName: types.AsQueryMethod<typeof findByName>;
 }
 
 function findByName(this: types.QueryHelperThis<typeof Person, QueryHelpers>, name: string) {
-    return this.find({ name })
+  return this.find({ name })
 }
 
 class Address {
-    @prop()
-    line1: string
+  @prop()
+  line1: string
+
+  constructor(line1: string) {
+    this.line1 = line1;
+  }
 }
 
 @queryMethod(findByName)
 class Person {
-    @prop()
-    public name: string
-    @prop()
-    public addresses: Address[];
+  @prop()
+  public name: string
+
+  // As a sub-doc
+  // @prop({ type: () => Address })
+  // As a mixed type
+  @prop()
+  public addresses: Address[];
+
+  constructor(name: string, addresses: Address[]) {
+    this.name = name;
+    this.addresses = addresses;
+  }
 }
 
 (async () => {
-    const mongod = await MongoMemoryServer.create();
-    const uri = mongod.getUri();
+  const mongod = await MongoMemoryServer.create();
+  const uri = mongod.getUri();
 
-    await mongoose.connect(uri, { dbName: 'test' });
+  await mongoose.connect(uri, { dbName: 'test' });
 
-    const PersonModel = getModelForClass<typeof Person,
-        QueryHelpers>(Person);
+  const PersonModel = getModelForClass<typeof Person, QueryHelpers>(Person);
 
-    await PersonModel.create({
-        name: 'hello',
-        addresses: [
-            {
-                line1: 'line 1'
-            }, {
-                line1: 'line 2'
-            }
-        ]
-    });
+  await PersonModel.create({ name: 'n1', addresses: [{ line1: 'a1' }, { line1: 'a2' }] })
+  await PersonModel.create(new Person('n2', [new Address('a3'), new Address('a4')]));
 
-    const allDocs = await PersonModel.find()
+  console.log('Find one lean POJO')
+  let pojo = await PersonModel.findOne().lean()
+  console.log(pojo, pojo.addresses[0].line1)
 
-    const docs: types.DocumentType<typeof Person, QueryHelpers>[] = await PersonModel.find().findByName('hello');
+  console.log('Find all docs')
+  const allDocs =  await PersonModel.find().lean()
 
-    console.log(docs[0].name);
-    console.log(allDocs[0].name);
-    //console.log(docs[0].addresses[0].line1);
-    console.log((docs[0] as any).addresses[0].line1)
-    console.log(allDocs[0].addresses[0].line1)
+  for (let doc of allDocs) {
+    // For when addresses is a sub-doc
+    // let addresses = (await doc).addresses;
+    // console.log(doc), addresses)
+
+    // For when addresses are a mixed type
+    console.log(doc, (await doc).addresses[0].line1)
+  }
+
+  console.log('Find docs with name of `n1`')
+  const docs = await PersonModel.find().findByName('n1');
+
+  for (let doc of docs) {
+    // For when addresses is a sub-doc
+    // let addresses = (await doc).addresses;
+    // console.log(doc), addresses)
+
+    // For when addresses are a mixed type
+
+    // Type error
+    // console.log(doc, (await doc).addresses[0].line1)
+
+    const docCasted = (doc as any) as mongoose.Document<any, QueryHelpers> & Person & IObjectWithTypegooseFunction & { _id: (mongoose.Document<any, QueryHelpers> & Person & IObjectWithTypegooseFunction)["_id"] }
+    console.log(doc, (await docCasted).addresses[0].line1)
+  }
+
+  process.exit(0)
 })();
